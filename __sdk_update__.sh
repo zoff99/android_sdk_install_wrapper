3c377289ef48945f8ee0a68a80348490e8e43be6
Zoff <zoff99@users.noreply.github.com>
2016-09-08 11:11:17 +0200

    Update __sdk_update__.sh

--- __sdk_update__.sh
+++ __sdk_update__.sh
@@ -1,4 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Wrapper for "android sdk" script installer
+# Copyright (C) 2016  Zoff <zoff@zoff.cc>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
 tmpfile="/tmp/sdk_update.$$.txt"
 rm -f "$tmpfile"
